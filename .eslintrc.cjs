4f2acd1556161ee8461cfacaf150c3390259c022
Bruce Estivil <baestivil@gmail.com>
2024-05-29 08:29:42 +0100

    push to vercel 1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ const config = {
   "rules": {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    "react/no-unescaped-entities": 0,
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
